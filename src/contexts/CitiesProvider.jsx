56751e05213c66e247e23f8685bfa5e00731dcc8
karinatimm <kartimm.webdev@gmail.com>
2025-03-03 22:01:46 +0100

    Try changes

--- src/contexts/CitiesProvider.jsx
+++ src/contexts/CitiesProvider.jsx
@@ -78,7 +78,7 @@ const CitiesProvider = ({ children }) => {
 
   const getCity = useCallback(
     async function getCity(id) {
-      if (Number(id) === currentCity.id) return;
+      if (!currentCity || Number(id) === currentCity.id) return;
 
       dispatch({ type: "loading" });
 
@@ -93,7 +93,7 @@ const CitiesProvider = ({ children }) => {
         });
       }
     },
-    [currentCity.id]
+    [currentCity]
   );
 
   const createCity = async (newCity) => {
